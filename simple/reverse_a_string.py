da815bc143221f1433fa5efacb03604f12d46570
LarsEier <39220022+LarsEier@users.noreply.github.com>
2018-05-12 18:44:12 +0300

    added reverse a string

---
+++ simple/reverse_a_string.py
@@ -0,0 +1,5 @@
+def reverse_string(string):
+	string = string[::-1] #[begin:end:step] step = 1 runs through the string normally, -1 reverses that.
+	print(string)
+
+reverse_string("This is a string. Hello World!")
